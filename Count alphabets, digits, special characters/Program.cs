95640d318918e86be0beccc2ddffe8807bed7b7e
Sachin Gurung <saching061@hotmail.com>
2019-12-10 22:57:55 -0800

    first commit

--- Count alphabets, digits, special characters/Program.cs
+++ Count alphabets, digits, special characters/Program.cs
@@ -1,15 +1,51 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Count_alphabets__digits__special_characters
+public class Exercise7
 {
-    class Program
+    public static void Main()
     {
-        static void Main(string[] args)
+        string str;
+        int alp, digit, splch, i, l;
+        alp = digit = splch = i = 0;
+
+
+        Console.Write("\n\nCount total number of alphabets, digits and special characters :\n");
+        Console.Write("--------------------------------------------------------------------\n");
+        Console.Write("Input the string : ");
+        str = Console.ReadLine();
+        l = str.Length;
+
+        /* Checks each character of string*/
+
+        while (i < l)
         {
+            if ((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z'))
+            {
+                alp++;
+            }
+            else if (str[i] >= '0' && str[i] <= '9')
+            {
+                digit++;
+            }
+            else
+            {
+                splch++;
+            }
+
+            i++;
         }
+
+        Console.Write("Number of Alphabets in the string is : {0}\n", alp);
+        Console.Write("Number of Digits in the string is : {0}\n", digit);
+        Console.Write("Number of Special characters in the string is : {0}\n\n", splch);
     }
 }
+/*
+Count total number of alphabets, digits and special characters :
+--------------------------------------------------------------------
+Input the string : hi there. )(
+Number of Alphabets in the string is : 7
+Number of Digits in the string is : 0
+Number of Special characters in the string is : 5
+
+Press any key to continue . . .
+*/
